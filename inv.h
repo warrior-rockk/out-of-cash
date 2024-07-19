c76763473a7b7d2e32d67d828330bf2a939259b9
warrior-rockk <warrior.rockk@gmail.com>
2024-07-19 16:16:16 +0200

    added inventory data file

---
+++ inv.h
@@ -0,0 +1,8 @@
+/* Allegro datafile object indexes, produced by grabber v4.2.1, MSVC */
+/* Datafile: d:\Instalables\Proyectos\Programacion DOS\entorno DGK4DOS\env\code\game\inv.dat */
+/* Date: Fri Jul 19 16:01:58 2024 */
+/* Do not hand edit! */
+
+#define dInv_Cassette                    0        /* BMP  */
+#define dInv_COUNT                       1
+
