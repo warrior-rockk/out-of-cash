07b5084360a4973e20a903a8744a45915b0db9c7
warrior-rockk <warrior.rockk@gmail.com>
2024-08-30 14:26:58 +0200

    added art

--- objects.h
+++ objects.h
@@ -1,13 +1,16 @@
 /* Allegro datafile object indexes, produced by grabber v4.2.1, MSVC */
 /* Datafile: d:\Instalables\Proyectos\Programacion DOS\entorno DGK4DOS\env\code\game\objects.dat */
-/* Date: Mon Aug 26 15:46:45 2024 */
+/* Date: Fri Aug 30 14:26:46 2024 */
 /* Do not hand edit! */
 
-#define dObjBathDoor                     0        /* BMP  */
-#define dObjCassette                     1        /* BMP  */
-#define dObjGuitar                       2        /* BMP  */
-#define dObjStereo01                     3        /* BMP  */
-#define dObjStereo02                     4        /* BMP  */
-#define dObjStereo03                     5        /* BMP  */
-#define dObjStereo04                     6        /* BMP  */
+#define dObjBathCloset                   0        /* BMP  */
+#define dObjBathDoor                     1        /* BMP  */
+#define dObjBathDoor2                    2        /* BMP  */
+#define dObjBathGel                      3        /* BMP  */
+#define dObjCassette                     4        /* BMP  */
+#define dObjGuitar                       5        /* BMP  */
+#define dObjStereo01                     6        /* BMP  */
+#define dObjStereo02                     7        /* BMP  */
+#define dObjStereo03                     8        /* BMP  */
+#define dObjStereo04                     9        /* BMP  */
 
