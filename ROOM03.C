ce79b00f93d315cff8e26230a5727f2dbd47a78f
warrior-rockk <warrior.rockk@gmail.com>
2024-10-28 19:31:18 +0100

    roomgen tool updated and working on room03

--- ROOM03.C
+++ ROOM03.C
@@ -1,8 +1,7 @@
 /*
 =========================================================
  Room Number:   03
- Room Name:     bathRoom
-
+ Room Name:     03_bathroom
 =========================================================
 */
 
@@ -13,19 +12,35 @@
 #include "engine.h"
 #include "room03.h"
 
-//#include "IDATA.h"
-
 //Funtion to return the name of hotspot by color code
 void r03_get_hotspot_name(uint8_t colorCode, char *s)
 {
     //check the object
     switch(colorCode)
     {
-        case r03_Closet:
-            strcpy(s, "Armario");
+        case r03_closet:
+                strcpy(s, "Armario");
+            break;
+        case r03_washbowl:
+                strcpy(s, "Lavabo");
+            break;
+        case r03_mirror:
+                strcpy(s, "Espejo");
+            break;
+        case r03_towel:
+                strcpy(s, "Toalla");
+            break;
+        case r03_gel:
+                strcpy(s, "Gel");
+            break;
+        case r03_bathMat:
+                strcpy(s, "Alfombrilla");
+            break;
+        case r03_switch:
+                strcpy(s, "Interruptor");
             break;
         case r03_door:
-            strcpy(s, "Puerta");
+                strcpy(s, "Puerta");
             break;
         default:
             strcpy(s, "");
@@ -38,7 +53,28 @@ enum verbs r03_get_default_hotspot_verb(uint8_t colorCode)
     //check the object
     switch(colorCode)
     {
-        case r03_Closet:
+        case r03_closet:
+            return LOOK;
+            break;
+        case r03_washbowl:
+            return USE;
+            break;
+        case r03_mirror:
+            return LOOK;
+            break;
+        case r03_towel:
+            return LOOK;
+            break;
+        case r03_gel:
+            return LOOK;
+            break;
+        case r03_bathMat:
+            return LOOK;
+            break;
+        case r03_switch:
+            return USE;
+            break;
+        case r03_door:
             return OPEN;
             break;
         default:
@@ -50,7 +86,7 @@ enum verbs r03_get_default_hotspot_verb(uint8_t colorCode)
 tObject* r03_get_object_info(uint8_t numObject)
 {
     if (numObject < R03_ROOM_NUM_OBJS)
-        return NULL; //&r03_object[numObject];
+        return &r03_object[numObject];
     else
         return NULL;
 }
@@ -58,12 +94,6 @@ tObject* r03_get_object_info(uint8_t numObject)
 //function to init room
 void r03_room_init()
 {
-    //update room objects state
-    /*
-    r03_object[r03_CASSETTE_OBJ_ID].active  = !is_game_flag(GOT_CASSETTE);
-    r03_object[r03_GUITAR_OBJ_ID].active    = !is_game_flag(GOT_GUITAR);
-    r03_object[r03_STEREO_OBJ_ID].active    = is_game_flag(STEREO_ON);
-    */
     game_fade_in();
 }
 
@@ -72,6 +102,9 @@ void r03_room_update()
 {
     //update room objects
     r03_update_room_objects();
+
+    //update dialog line selection
+    r03_update_dialog_selection();
     
     //update room script
     r03_update_room_script();
@@ -80,22 +113,25 @@ void r03_room_update()
 //update room objects
 void r03_update_room_objects()
 {
-    //update objects
 
-    //Stereo
-    //object_play_animation(&r03_object[r03_STEREO_OBJ_ID], r03d_objStereo01, r03_animations, r03_ANIM_PLAY_STEREO);
 }
 
-//updates room script
+//update dialog selection
+void r03_update_dialog_selection()
+{
+
+}
+
+//update room script
 void r03_update_room_script()
 {
-    //if script active is room script type
+//if script active is room script type
     if (roomScript.active && roomScript.type == ROOM_SCRIPT_TYPE)
     {
         //sequence actions
         switch (roomScript.object)
         {
-            case r03_Closet:
+            case r03_closet:
                 switch(roomScript.verb)
                 {
                     case LOOK:
@@ -103,33 +139,135 @@ void r03_update_room_script()
                         {
                             case 0:
                                 begin_script();
-                                script_say("El armario del ba"CHAR_NY"o");
+                                script_say("Armario");
                                 break;
                             default:
                                 end_script();
                                 break;
                         }
-                    break;
+                    break;                    
                 }
-            break;
-            case r03_door:
+                break;            
+            case r03_washbowl:
                 switch(roomScript.verb)
                 {
-                    case GO:
+                    case LOOK:
                         switch (roomScript.step)
                         {
                             case 0:
                                 begin_script();
-                                script_move_player_to_target();
+                                script_say("Lavabo");
                                 break;
-                            case 1:
-                                change_room_pos(1, 154, 46);
+                            default:
                                 end_script();
                                 break;
                         }
-                    break;
+                    break;                    
                 }
-            break;
+                break;            
+            case r03_mirror:
+                switch(roomScript.verb)
+                {
+                    case LOOK:
+                        switch (roomScript.step)
+                        {
+                            case 0:
+                                begin_script();
+                                script_say("Espejo");
+                                break;
+                            default:
+                                end_script();
+                                break;
+                        }
+                    break;                    
+                }
+                break;            
+            case r03_towel:
+                switch(roomScript.verb)
+                {
+                    case LOOK:
+                        switch (roomScript.step)
+                        {
+                            case 0:
+                                begin_script();
+                                script_say("Toalla");
+                                break;
+                            default:
+                                end_script();
+                                break;
+                        }
+                    break;                    
+                }
+                break;            
+            case r03_gel:
+                switch(roomScript.verb)
+                {
+                    case LOOK:
+                        switch (roomScript.step)
+                        {
+                            case 0:
+                                begin_script();
+                                script_say("Gel");
+                                break;
+                            default:
+                                end_script();
+                                break;
+                        }
+                    break;                    
+                }
+                break;            
+            case r03_bathMat:
+                switch(roomScript.verb)
+                {
+                    case LOOK:
+                        switch (roomScript.step)
+                        {
+                            case 0:
+                                begin_script();
+                                script_say("Alfombrilla");
+                                break;
+                            default:
+                                end_script();
+                                break;
+                        }
+                    break;                    
+                }
+                break;            
+            case r03_switch:
+                switch(roomScript.verb)
+                {
+                    case LOOK:
+                        switch (roomScript.step)
+                        {
+                            case 0:
+                                begin_script();
+                                script_say("Interruptor");
+                                break;
+                            default:
+                                end_script();
+                                break;
+                        }
+                    break;                    
+                }
+                break;            
+            case r03_door:
+                switch(roomScript.verb)
+                {
+                    case LOOK:
+                        switch (roomScript.step)
+                        {
+                            case 0:
+                                begin_script();
+                                script_say("Puerta");
+                                break;
+                            default:
+                                end_script();
+                                break;
+                        }
+                    break;                    
+                }
+                break;            
+
         }
     }
 }
