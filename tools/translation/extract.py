8514276ecec01c96cbc7bd3377718c6165e57b99
warrior-rockk <warrior.rockk@gmail.com>
2025-02-14 21:01:35 +0100

    updated tool extract

--- tools/translation/extract.py
+++ tools/translation/extract.py
@@ -1,8 +1,11 @@
 import re
+import argparse
+import os
 
 def extraer_cadenas_con_comillas(archivo_entrada, archivo_salida):
     """
-    Extrae las cadenas de texto entre comillas de un archivo y las guarda en otro.
+    Extrae las cadenas de texto entre comillas (incluyendo escapadas) de un archivo y las guarda en otro,
+    ignorando las cadenas vacías.
 
     Args:
         archivo_entrada: La ruta al archivo de texto de entrada.
@@ -12,17 +15,27 @@ def extraer_cadenas_con_comillas(archivo_entrada, archivo_salida):
     try:
         with open(archivo_entrada, 'r') as f_entrada, open(archivo_salida, 'w') as f_salida:
             for linea in f_entrada:
-                # Encuentra todas las cadenas entre comillas en la línea actual
-                cadenas = re.findall(r'"([^"]*)"', linea)  
+                # Encuentra todas las cadenas entre comillas (escapadas o no) en la línea actual
+                cadenas = re.findall(r'"([^"\\]*(?:\\.[^"\\]*)*)"', linea)  
                 for cadena in cadenas:
-                    f_salida.write(cadena + '\n')
+                    if cadena:  # Verifica si la cadena no está vacía
+                        f_salida.write(cadena + ',' + '\n')
     except FileNotFoundError:
         print(f"Error: Uno o ambos archivos no fueron encontrados.")
     except Exception as e:
         print(f"Ocurrió un error: {e}")
 
-# Ejemplo de uso:
-archivo_entrada = 'room01.c'  # Reemplaza con la ruta a tu archivo de entrada
-archivo_salida = 'salida.txt'  # Reemplaza con la ruta donde quieres guardar las cadenas
+# Argumentos de entrada
+parser = argparse.ArgumentParser(description="Extrae cadenas de texto entre comillas (ignorando las cadenas vacías) y las guarda otro archivo")
+parser.add_argument("-f", "--file", help="Nombre del archivo a extraer las cadenas de texto")
+parser.add_argument("-o", "--output", help="Nombre del archivo destino donde depositar las cadenas extraídas")
+args = parser.parse_args()
 
-extraer_cadenas_con_comillas(archivo_entrada, archivo_salida)
+# Llamada a funcion con argumentos de entrada
+if args.file:
+    if args.output:
+        extraer_cadenas_con_comillas(args.file, args.output)
+    else:
+        extraer_cadenas_con_comillas(args.file, os.path.splitext(args.file)[0] + ".csv")
+else:
+    print(f"No se ha especificado archivo de entrada")
